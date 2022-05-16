a90d47b0ac51b10745ca15ac31989215dc0718e3
오승은 <tmddms927@gmail.com>
2022-05-16 23:47:01 +0900

    05/16

---
+++ ft_strmapi.c
@@ -0,0 +1,24 @@
+
+#include "libft.h"
+
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	size_t	i;
+	size_t	len;
+	char	*str;
+
+	if (!s || !f)
+		return (0);
+	len = ft_strlen(s);
+	str = (char *)malloc(sizeof(char) * (len + 1));
+	if (!str)
+		return (0);
+	i = 0;
+	while (s[i] != '\0')
+	{
+		str[i] = f(i, s[i]);
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
+}
