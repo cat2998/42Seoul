c6dd4215b0863e4cc8e60a64b458d5e000357c18
오승은 <tmddms927@gmail.com>
2022-05-17 00:02:05 +0900

    homework

---
+++ ft_putnbr_fd.c
@@ -0,0 +1,27 @@
+
+#include "libft.h"
+
+void	ft_putnbr_fd(int n, int fd)
+{
+	char	str;
+
+	if (n == -2147483648)
+	{
+		ft_putnbr_fd(n / 10, fd);
+		write(fd, "8", 1);
+		return ;
+	}
+	else if (n < 0)
+	{
+		write(fd, "-", 1);
+		n *= -1;
+	}
+	if (n >= 10)
+	{
+		ft_putnbr_fd(n / 10, fd);
+		str = n % 10 + 48;
+	}
+	else
+		str = n + 48;
+	write(fd, &str, 1);
+}
