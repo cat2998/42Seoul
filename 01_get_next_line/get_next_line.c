9b8a5a8faa41169db27f78bc6be00cffde76d21f
jihyun <cat2998@naver.com>
2022-07-07 18:01:37 +0900

    0707

--- 01_get_next_line/get_next_line.c
+++ 01_get_next_line/get_next_line.c
@@ -11,27 +11,29 @@
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-
+#include <stdio.h>
 char	*ft_read_to_newline(int fd, char *sum)
 {
 	char	*buf;
 	int		read_size;
-
+	//printf("1");
 	buf = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
 	if(!buf)
 		return (0);
-	read_size = 1;
+	read_size = 1;//printf("2");
 	while (ft_strchr(sum, '\n') < 0 && read_size != 0)
-	{
+	{//printf("3");
 		read_size = read(fd, buf, BUFFER_SIZE);
 		if (read_size == -1)
-		{
+		{//printf("4");
 			free(buf);
 			return (0);
 		}
 		buf[read_size] = '\0';
 		sum = ft_strjoin(sum, buf);
-	}
+	}//printf("5");
+	free(buf);
+	//printf("??sum: %s\n", sum);
 	return (sum);
 }
 
@@ -54,7 +56,7 @@ char	*ft_get_line(char *sum, int n)
 	if(!result)
 		return (0);
 	i = 0;
-	while (i < n + 1)
+	while (sum[i] != '\0' && i < n + 1)
 	{
 		result[i] = sum[i];
 		i++;
@@ -68,8 +70,8 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	size_t	i;
 	char	*str;
 
-	if (!s)
-		return (0);
+	// if (len == 0)
+	// 	return (0);
 	// if (start > ft_strlen(s))
 	// 	return (ft_strdup(""));
 	// if (start + len > ft_strlen(s))
@@ -78,27 +80,37 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	if (!str)
 		return (0);
 	i = 0;
-	while (i < len)
+	while (s[start + i] != '\0' && i < len)
 	{
-		str[i] = s[start + i];
+		str[i] = s[start + 1 + i];
 		i++;
 	}
+	str[i] = '\0';
 	return (str);
 }
-
+#include <stdio.h>
 char	*get_next_line(int fd)
 {
 	int			i;
 	char		*line;
 	static char	*sum;
-
-	sum = ft_read_to_newline(fd, sum);//printf("sum : %s\n", sum);
+	//printf("sum : ||%s||\n", sum);
+	sum = ft_read_to_newline(fd, sum);//printf("read sum : ||%s||\n", sum);
 	if (!sum)
+	{
+		printf("here?1111");
 		return (0);
+	}
 	i = ft_find_newline(sum);//printf("i : %d\n", i);
 	line = ft_get_line(sum, i);//printf("line : %s\n", line);
-	sum = ft_substr(sum, i + 1, ft_strlen(sum) - i);
-	//printf("after : %s\n", sum);
+	sum = ft_substr(sum, i, ft_strlen(sum) - i);
+	//printf("strlen(sum) : %I64d\n", ft_strlen(sum));
+	if (sum[0] == '\0' && i == 0)
+	{
+		printf("here?2222");
+		return (0);
+	}
+	//printf("after : ||%s||\n", sum);
 	return (line);
 }
 
