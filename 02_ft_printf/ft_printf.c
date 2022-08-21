067bb2736a3fc18dd2ef36185ce439709125abb1
jihyun <cat2998@naver.com>
2022-08-21 19:26:00 +0900

    0821

--- 02_ft_printf/ft_printf.c
+++ 02_ft_printf/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: jgwon <jgwon@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/17 21:08:38 by jgwon             #+#    #+#             */
-/*   Updated: 2022/08/21 18:22:55 by jgwon            ###   ########.fr       */
+/*   Updated: 2022/08/21 19:25:03 by jgwon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,26 +22,60 @@ typedef struct s_info
 	int	sharp;
 	int	width;
 	int precision;
+	int pn;
 	char	type;
 }	t_info;
 
+const char *ft_info(const char *format, va_list ap)
+{
+	while (*format != 'd')
+	{
+		// ft_checkflag(format);
+		printf("%c", *format);
+		format++;
+	}
+	return format;
+}
+
+int	ft_myprintf(va_list ap, const char *format)
+{
+	// int	a;
+	// char	*p;
+
+	while (*format != 0)
+	{
+		if (*format == '%')
+		{
+			format++;
+			format = ft_info(format, ap);
+		}
+		else
+		{
+			printf("%c", *format);
+		}
+		format++;
+	}
+
+	// for (int i = 0; i < 3; i++)
+	// {
+	// 	a = va_arg(ap, int);
+	// 	printf("%d\n", a);
+	// }
+	return 0;
+}
+
 int	ft_printf(const char *format, ...)
 {
-	int		a;
 	va_list	ap;
 
 	va_start(ap, format);
-	for (int i = 0; i < 3; i++)
-	{
-		a = va_arg(ap, int);
-		printf("%d||\n", a);
-	}
+	ft_myprintf(ap, format);
 	va_end(ap);
 
-	return a;
+	return 0;
 }
 
 int main(void)
 {
-	
+	ft_printf("Hello%dWorld%d!!!%d", 10, 20, 30);
 }
