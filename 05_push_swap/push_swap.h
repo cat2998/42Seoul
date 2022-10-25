c2c785a372ba5b241cdb2f00a7acef119b81617b
jihyun <55981401+cat2998@users.noreply.github.com>
2022-10-25 22:11:04 +0900

    1025

---
+++ 05_push_swap/push_swap.h
@@ -0,0 +1,20 @@
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+typedef struct s_node
+{
+	int				value;
+	struct s_node	*prev;
+	struct s_node	*next;
+}	t_node;
+
+typedef struct s_stack
+{
+	int		size;
+	t_node	*top;
+	t_node	*bottom;
+}	t_stack;
+
+void    init_stack(t_stack s);
+
+#endif
