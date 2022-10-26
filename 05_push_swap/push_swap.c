f7cb5230550b383f4f1f85e80907963c253ba0a3
jihyun <cat2998@naver.com>
2022-10-26 23:24:30 +0900

    1026

--- 05_push_swap/push_swap.c
+++ 05_push_swap/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: jgwon <jgwon@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/23 15:54:46 by jgwon             #+#    #+#             */
-/*   Updated: 2022/10/23 16:37:00 by jgwon            ###   ########.fr       */
+/*   Updated: 2022/10/26 23:11:02 by jgwon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,58 +15,72 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void print_error(void)
-{
-    write(1, "Error\n", 6);
-    return ;
-}
-
-long ft_atoi(char *argv)
+void ft_atoi(char *argv)
 {
     int i;
     int minus;
+    int flag;
     long result;
 
     i = 0;
     minus = 0;
+    flag = 0;
     result = 0;
-    if (argv[0] == '-')
-    {
-        i = 1;
-        minus = 1;
-    }
     while (argv[i] != '\0')
     {
-        if (argv[i] == ' ')
+        if (argv[i] == ' ' && flag == 1)
+        {
+            if (minus == 1)
+                result *= -1;
+            if (-2147483648 <= result && result <= 2147483647)
+                //push stack
+            flag = 0;
             result = 0;
+        }
+        else if ((argv[i] == '+' || argv[i] == '-') && flag == 0)
+        {
+            if (argv[i] == '-')
+                minus = 1;
+            result = 0;
+        }
         else if (48 <= argv[i] && argv[i] <= 57)
+        {
             result = result * 10 + argv[i] - 48;
+            flag = 1;
+        }
         else
         {
-            write(1, "1", 1);
+            write(1, "Error\n", 6);
+            //free
             exit(0);
         }
         i++;
     }
-    if (minus == 1)
-        result *= -1;
-    if (-2147483648 <= result && result <= 2147483647)
-        return result;
-    else
-        exit(0);
+    if (flag == 1)
+    {
+        if (minus == 1)            
+            result *= -1;
+        if (-2147483648 <= result && result <= 2147483647)
+        {
+            //push stack
+        }
+    }
+    return ;
 }
 
 int main(int argc, char *argv[])
 {
     t_stack *stackA;
 
-    atexit(print_error);
+    stackA = malloc(sizeof(t_stack) * 1);
+	if (!stackA)
+		return (0);
     init_stack(stackA);
-
     while (argc > 1)
     {
         ft_atoi(argv[argc - 1]);
         argc--;
     }
+    return (0);
     // quick_sork()
 }
